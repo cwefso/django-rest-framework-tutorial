6d18481d2c106819ad4a502070b3e9998707db0b
Charles Wefso <cwefso@gmail.com>
2021-01-26 13:30:04 -0700

    Completes Django Rest Framework Tutorial Part 6

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2zvxu&$844_rj0)^n=yr(kklu1!5u&-j--&4(e=%@exv!_=q1w'
-
+SECRET_KEY = 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
